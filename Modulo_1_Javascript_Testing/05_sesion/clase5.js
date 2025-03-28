236f7c5e8adbc64eee9409e60f96cfe910f69496
Roberto Carvajal de PaZ <rcarvajal@megacable.com.mx>
2025-03-28 10:09:35 -0600

    clase día 5, 28/03/2025
    rcarvajal

---
+++ Modulo_1_Javascript_Testing/05_sesion/clase5.js
@@ -0,0 +1,8 @@
+const titulo = document.getElementById("titulo")
+const parrafo = document.getElementById("parrafo");
+
+console.log(titulo);
+console.log(titulo.textContent);
+
+titulo.textContent = "Bienvenido";
+parrafo.style.color = "blue";
