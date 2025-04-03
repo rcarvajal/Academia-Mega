5ea740fab0b3dc3d6d36a53c1ee03e87a4d50571
Roberto Carvajal de PaZ <rcarvajal@megacable.com.mx>
2025-04-03 10:04:47 -0600

    se actualizan actividades de la session 8 y 9.  meet del día 03/04/2025
    rcarvajal

---
+++ Modulo_1_Javascript_Testing/09_sesion/main.js
@@ -0,0 +1,11 @@
+import {saludar, despedir, suma, resta, multiplicar, dividir} from "./saludo.js";
+
+const nombre = "Ulises";
+
+console.log(saludar(nombre));
+console.log(despedir(nombre));
+
+console.log("suma(1,2): ", suma(1,2));
+console.log("resta(1,2): ", resta(1,2));
+console.log("multiplilcar(1,2): ", multiplicar(1,2));
+console.log("dividir(2,2): ", dividir(2,2));
---
+++ Modulo_1_Javascript_Testing/09_sesion/saludo.js
@@ -0,0 +1,23 @@
+export function saludar(nombre) {
+    return `Hola, ${nombre}! Bienvenido de módulos en Javascript`;
+}
+
+export function despedir(nombre) {
+    return `Adios, ${nombre}! Nos vemos en la próxima clase`;
+}
+
+export function suma (a, b) {
+    return a + b;
+}
+
+export function resta (a, b) {
+    return a - b;
+}
+
+export function multiplicar (a, b) {
+    return a * b;
+}
+
+export function dividir (a, b) {
+    return a / b;
+}
