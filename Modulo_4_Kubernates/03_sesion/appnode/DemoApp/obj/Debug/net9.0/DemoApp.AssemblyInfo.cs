b77218812d16883423c005c3018f0a1979afc71d
Roberto Carvajal de PaZ <rcarvajal@megacable.com.mx>
2025-06-26 18:02:46 -0600

    tarea jueves 26062025
    rcarvajal

--- Modulo_4_Kubernates/03_sesion/appnode/DemoApp/obj/Debug/net9.0/DemoApp.AssemblyInfo.cs
+++ Modulo_4_Kubernates/03_sesion/appnode/DemoApp/obj/Debug/net9.0/DemoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4920a1037cba0f87b8c9d534c187045037a880d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff688a8a4ea3562427bd09505857f4651cff606f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
