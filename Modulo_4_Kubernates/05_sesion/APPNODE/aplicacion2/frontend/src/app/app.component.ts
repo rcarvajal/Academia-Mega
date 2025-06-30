e4f96313aa5040ee3a34c4966281e98000d1beb8
Roberto Carvajal de PaZ <rcarvajal@megacable.com.mx>
2025-06-30 01:06:56 -0600

    clase 27/06/2025
    rcarvajal

---
+++ Modulo_4_Kubernates/05_sesion/APPNODE/aplicacion2/frontend/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ItemListComponent } from "./components/item-list/item-list.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [ItemListComponent, CommonModule, BrowserModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'frontend';
+}
